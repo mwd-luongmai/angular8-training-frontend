59acc45045f35c5134f0ba177bef6b76d273570b
mwd-luongmai <luong.mai@mowede.com>
2021-05-27 02:16:59 +0000

    Updates

--- main-es5.js
+++ main-es5.js
@@ -614,7 +614,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     };
 
     var AppConfig = {
-      API_URL: 'https://swatang-api.herokuapp.com/',
+      API_URL: 'https://swatang-api.herokuapp.com',
       MatterpostSDKKey: 'db4d6653-164a-4cfc-87d5-4f38e0756512',
       MatterpostSDKVersion: '3.0'
     };
