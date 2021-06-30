897179c314a30515a7b3d7efb700e4a1114a5bb4
Duong Quang Loc <duongquangloc.it.93@gmail.com>
2021-07-01 06:35:07 +0700

    implement unit test

--- src/app/features/user/change-password/change-password.component.spec.ts
+++ src/app/features/user/change-password/change-password.component.spec.ts
@@ -1,112 +1,202 @@
-import { async, ComponentFixture, TestBed, tick, fakeAsync, flush } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http";
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed,
+  tick,
+} from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { UserService, AlertService, CustomMaterialModule } from "@app/core";
+import { AuthenticationService } from "../../../core/services/authentication/authentication.service";
+import { TestAuthenticationService } from "../../../core/services/testing/test-authentication.service";
+import { TestUserService } from "@app/core/services/testing/test-user.service";
+import { getTestUsers } from "@app/core/models/testing/test-users";
+import { throwError } from "rxjs";
+import { ChangePasswordComponent } from "./change-password.component";
+import { LoginComponent } from "@app/features/authentication/login/login.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SharedModule } from "@app/shared";
+import { configureTestSuite } from "src/setupTestBed";
+import { By } from "@angular/platform-browser";
 
-import { ForgotPasswordComponent } from './forgot-password.component';
-import { CustomMaterialModule } from '../../../core/material.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { SharedModule } from '../../../shared/shared.module';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HomePageComponent } from '@app/features/home/home-page/home-page.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AlertService } from '@app/core/services';
+let component: ChangePasswordComponent;
+let fixture: ComponentFixture<ChangePasswordComponent>;
 
-import { UserService } from '../../../core/services/user/user.service';
-import { TestUserService } from '../../../core/services/testing/test-user.service';
-import { throwError } from 'rxjs';
+describe("ChangePasswordComponent", () => {
+  configureTestSuite();
 
-describe('ChangePasswordComponent', () => {
-  let component: ForgotPasswordComponent;
-  let fixture: ComponentFixture<ForgotPasswordComponent>;
+  beforeAll((done) =>
+    (async () => {
+      TestBed.configureTestingModule({
+        declarations: [ChangePasswordComponent, LoginComponent],
+        imports: [
+          BrowserAnimationsModule,
+          SharedModule,
+          CustomMaterialModule,
+          ReactiveFormsModule,
+          RouterModule.forRoot([]),
+          HttpClientModule,
+          RouterTestingModule.withRoutes([
+            { path: "login", component: LoginComponent },
+          ]),
+        ],
+        providers: [
+          ChangePasswordComponent,
+          {
+            provide: AuthenticationService,
+            useClass: TestAuthenticationService,
+          },
+          { provide: UserService, useClass: TestUserService },
+        ],
+      });
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent, HomePageComponent ],
-      imports: [
-        CustomMaterialModule,
-        ReactiveFormsModule,
-        FormsModule,
-        SharedModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        RouterTestingModule.withRoutes([ { path: '', component: HomePageComponent } ])
-      ],
-      providers: [
-        {provide: UserService, useClass: TestUserService}
-      ]
-    })
-    .compileComponents().then(() => {
-      fixture = TestBed.createComponent(ForgotPasswordComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
+      await TestBed.compileComponents();
+    })()
+      .then(done)
+      .catch(done.fail)
+  );
+
+  describe("ChangePasswordComponent", () => {
+    beforeEach(async(async () => {
+      createComponent();
+    }));
+
+    it("should create", () => {
+      expect(component).toBeTruthy();
     });
-  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it("should load current user data", () => {
+      const testUser = getTestUsers()[0];
+      expect(component.currentUser.name).toEqual(testUser.name);
+    });
+
+    it("should not attempt to change password account with an empty password", fakeAsync(() => {
+      const userServiceSpy = fixture.debugElement.injector.get(UserService);
+      const changePasswordSpy = spyOn(
+        userServiceSpy,
+        "changePassword"
+      ).and.callThrough();
+
+      component.formControls.password.setValue("");
+
+      component.onSubmit();
+      tick();
+
+      expect(changePasswordSpy.calls.any()).toBe(
+        false,
+        "UserService.changePassword should not be called"
+      );
+      expect(component.loading).toBe(false, "Loading status should be false");
+      expect(component.submitted).toBe(true, "Submitted status should be true");
+    }));
+
+    it("form should be invalid with length of password is less than 8 characters", () => {
+      component.formControls.password.setValue("123456");
+
+      expect(component.changePassForm.invalid).toBeTruthy();
+    });
+
+    it("form should be invalid with length of password does not contains least 4 alphabetic characters", () => {
+      component.formControls.password.setValue("ab123456");
+
+      expect(component.changePassForm.invalid).toBeTruthy();
+    });
+
+    it("form should be invalid with length of password does not contain at least 1 non-alphabetic character", () => {
+      component.formControls.password.setValue("abcdefgh");
+
+      expect(component.changePassForm.invalid).toBeTruthy();
+    });
+
+    it("should not attempt to change password with an invalid password", fakeAsync(() => {
+      const userServiceSpy = fixture.debugElement.injector.get(UserService);
+      const deleteAccountSpy = spyOn(
+        userServiceSpy,
+        "changePassword"
+      ).and.callThrough();
+
+      component.formControls.password.setValue("123");
+
+      component.onSubmit();
+      tick();
+      expect(component.submitted).toBe(true, "Submitted status should be true");
+
+      expect(deleteAccountSpy.calls.any()).toBe(
+        false,
+        "UserService.changePassword should not be called"
+      );
+
+      expect(component.loading).toBe(false, "Loading status should be false");
+      expect(component.submitted).toBe(true, "Submitted status should be true");
+    }));
+
+    it("should call api with correct password format", fakeAsync(() => {
+      const userServiceSpy = fixture.debugElement.injector.get(UserService);
+      const alertServiceSpy = fixture.debugElement.injector.get(AlertService);
+      const alertSuccessSpy = spyOn(alertServiceSpy, 'success').and.callThrough();
+      const changePasswordSpy = spyOn(
+        userServiceSpy,
+        "changePassword"
+      ).and.callThrough();
+
+      component.formControls.oldPassword.setValue("1234mnbv1");
+      component.formControls.password.setValue("1234mnbv");
+      component.formControls.confirmPassword.setValue("1234mnbv");
+
+      component.onSubmit();
+      tick();
+  
+      expect(changePasswordSpy.calls.any()).toBe(
+        true,
+        "UserService.changePassword should be called"
+      );
+      expect(component.loading).toBe(false, "Loading status should be false");
+      expect(alertSuccessSpy.calls.any()).toBe(true, 'AlertService.success should be called');
+    }));
+
+    it("form should be valid with correct password format", fakeAsync(() => {
+      component.formControls.oldPassword.setValue("123453mnbv");
+      component.formControls.password.setValue("12345mnbv");
+      component.formControls.confirmPassword.setValue("12345mnbv");
+      expect(component.changePassForm.valid).toBeTruthy();
+      flush();
+    }));
+
+    it("should display an error when the password is wrong", fakeAsync(() => {
+      const userServiceSpy = fixture.debugElement.injector.get(UserService);
+      const changePasswordSpy = spyOn(
+        userServiceSpy,
+        "changePassword"
+      ).and.returnValue(throwError("UserService test failure"));
+
+      component.formControls.password.setValue("wrongP@sswo3d");
+
+      component.onSubmit();
+      tick();
+      expect(component.submitted).toBe(true, "Submitted status should be true");
+
+      expect(changePasswordSpy.calls.any()).toBe(
+        false,
+        "UserService.changePassword should not be called"
+      );
+
+      expect(component.loading).toBe(false, "Loading status should be true");
+      flush();
+    }));
   });
 
-  it('should set submmitted flag to true when submit form', () => {
-    component.onSubmit()
-    expect(component.submitted).toBeTruthy()
-  })
+  function createComponent() {
+    fixture = TestBed.createComponent(ChangePasswordComponent);
+    component = fixture.componentInstance;
 
-  it('form should be invalid with email is empty', () => {
-    component.formControls.email.setValue('')
-    expect(component.forgotPassForm.invalid).toBeTruthy()
-  })
+    fixture.detectChanges();
 
-  it('form should be invalid with length of invalid email format', () => {
-    component.formControls.email.setValue('test@gmail')
-    expect(component.forgotPassForm.invalid).toBeTruthy()
-  })
-
-
-  it('should call api when submit the form with valid data', fakeAsync(() => {
-    const userServiceSpy = fixture.debugElement.injector.get(UserService)
-    const alertServiceSpy = fixture.debugElement.injector.get(AlertService);
-    const alertSuccessSpy = spyOn(alertServiceSpy, 'success').and.callThrough();
-    const forgotSpy = spyOn(userServiceSpy, 'forgotPassWord').and.callThrough();
-    
-  
-    component.formControls.email.setValue('test123@gmail.com')
-    component.onSubmit()
-    tick()
-
-    expect(forgotSpy.calls.any()).toBe(true, 'UserService.forgotPassWord should be called');
-    expect(alertSuccessSpy.calls.any()).toBe(true, 'AlertService.success should be called');
-    expect(component.loading).toBe(false, 'Loading status should be false');
-    flush()
-  }))
-
-  it('should not call api when submit the form with invalid data', fakeAsync(() => {
-    const userServiceSpy = fixture.debugElement.injector.get(UserService)
-    const userSpy = spyOn(userServiceSpy, 'forgotPassWord').and.callThrough();
-   
-    component.formControls.email.setValue('')
-    component.onSubmit()
-    tick()
-
-    expect(userSpy.calls.any()).toBe(false, 'UserService.forgotPassWord should not be called');
-    expect(component.loading).toBe(false, 'Loading status should be false');
-    flush()
-  }))
-
-  it('should handle error when call api', fakeAsync(() => {
-    const userServiceSpy = fixture.debugElement.injector.get(UserService)
-    const forgotSpy = spyOn(userServiceSpy, 'forgotPassWord').and.callFake(() => {
-      return throwError(new Error('Fake error'));
+    return fixture.whenStable().then(() => {
+      fixture.detectChanges();
     });
-    const alertServiceSpy = fixture.debugElement.injector.get(AlertService);
-    const alertErrorSpy = spyOn(alertServiceSpy, 'error').and.callThrough();
-    
-    component.formControls.email.setValue('test123123@gmail.com')
-  
-    component.onSubmit()
-    tick()
-
-    expect(forgotSpy.calls.any()).toBe(true, 'UserService.signUp should be called');
-    expect(alertErrorSpy.calls.any()).toBe(true, 'AlertService.error should be called');
-    expect(component.loading).toBe(false, 'Loading status should be false');
-    flush()
-  }))
+  }
 });
