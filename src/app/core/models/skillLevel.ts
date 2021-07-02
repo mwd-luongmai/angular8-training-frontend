f1f31c8ef963728f308670dc5de8a0bad377c420
thien <thien1988@gmail.com>
2021-07-02 15:50:07 +0700

    fix unitest

--- src/app/core/models/skillLevel.ts
+++ src/app/core/models/skillLevel.ts
@@ -2,7 +2,7 @@ import { JsonProperty, JsonObject } from 'json2typescript'
 
 @JsonObject("SkillLevel")
 export class SkillLevel {
-    @JsonProperty("_id", String)
+    @JsonProperty("_id", String, true)
     _id: string = '';
 
     @JsonProperty("skillLevelName", String)
